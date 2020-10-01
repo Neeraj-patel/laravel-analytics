ae4d21c30b883e59e23ff686a25d7144810929e3
Neeraj-patel <40258588+Neeraj-patel@users.noreply.github.com>
2020-10-01 17:10:02 +0530

    Update analytics.php

--- config/analytics.php
+++ config/analytics.php
@@ -18,6 +18,7 @@ return [
      * The amount of minutes the Google API responses will be cached.
      * If you set this to zero, the responses won't be cached at all.
      */
+    
     'cache_lifetime_in_minutes' => 60 * 24,
 
     /*
